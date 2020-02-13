d2af294290441aaabb2d776a9bbf63452f0ede64
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-02-13 17:32:36 -0500

    Initial Pseudocode of the quiz

---
+++ script.js
@@ -0,0 +1,14 @@
+// Button generator
+
+// Quiz Function
+// Quiz starts up, with a question
+// A countdown timer starts at same time
+// Each Question requires three things:
+//  - Register a button press for one of the answers
+//  - If a question is answered correctly, add a certain amount of time to the countdown, and add to the final score
+//  - Else reduce the timer by a certain amount, and register the question as incorrectly answered
+// Either at the end of the quiz, or when the timer runs out, the score needs to displayed
+// An option to put up the score in a high score area
+// Option to do the quiz over
+
+// Button press Event Listener to start the quiz
